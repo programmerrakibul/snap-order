c977312409dec3f7f4cd62476e60610270b925c6
Programmer Rakibul <rakibul00206@gmail.com>
2026-09-01 19:10:52 +0600

    feat: update database schema by modifying Order and OrderItem models, adding new required fields, and removing obsolete ones

--- prisma.config.ts
+++ prisma.config.ts
@@ -11,7 +11,6 @@ export default defineConfig({
   schema: "prisma/",
   migrations: {
     path: "prisma/migrations",
-    seed: "tsx prisma/seed.ts",
   },
   datasource: {
     url: env("DATABASE_URL"),
